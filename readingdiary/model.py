b4bb45115c4098570068abcf607483c62e9daae5
JuanCHolguin <jholguin887@soyudemedellin.edu.co>
2026-03-15 22:11:59 -0500

    estructura basica y primer metodo de instancia

--- readingdiary/model.py
+++ readingdiary/model.py
@@ -1,3 +1,72 @@
 from datetime import datetime
 
-# TODO: Add code here
+class Note:
+    def __init__(self, text: str, page: int, date: datetime):
+        self.text: str= text
+        self.page: int = page
+        self.date: datetime = date
+
+    def __str__(self):
+        return f"{self.date} - page {self.page}: {self.text}"
+
+class Book:
+    EXCELLENT: int = 3
+    GOOD: int = 2
+    BAD: int = 1
+    UNRATED: int = -1
+
+    def __init__(self, isbn: str, title: str, author: str, pages: int):
+        self.isbn: str = isbn
+        self.title: str = title
+        self.author: str = author
+        self.pages: int = pages
+
+        self.rating: int = Book.UNRATED
+        self.notes: list[Note] = []
+
+    def add_note (self, text: str, page: int, date: datetime) -> bool:
+        if page > self.pages:
+            return False
+        else:
+            note = Note(text, page, date)
+            self.notes.append(note)
+        return True
+
+    def set_rating (self, rating: int) -> bool:
+        if rating != Book.EXCELLENT and rating != Book.GOOD and rating != Book.BAD:
+            return False
+
+    def get_notes_of_page (self, page: int) -> list[Note]:
+        pass
+
+    def page_with_most_notes (self) -> int:
+        pass
+
+    def __str__ (self) -> str:
+        return f"ISBN: {self.isbn}\nTitle: {self.title}\nAuthor: {self.author}\nPages: {self.pages}\nRating: {self.rating}"
+
+class ReadingDiary:
+    def __init__(self):
+        self.books: dict[str, Book] = {}
+
+    def add_book (self, isbn: str, title: str, author: str, pages: int) -> bool:
+        pass
+
+    def search_by_isbn (self, isbn: str) -> Book | None:
+        pass
+
+    def add_note_to_book (self, isbn: str, text: str, page: int, date: datetime) -> bool:
+        pass
+
+    def rate_book(self, isbn: str, rating: int) -> bool:
+        book = self.search_by_isbn(isbn)
+        if book is None:
+            return False
+        return book.set_rating(rating)
+
+    def book_with_most_notes (self) -> Book | None:
+        pass
+
+
+
+
